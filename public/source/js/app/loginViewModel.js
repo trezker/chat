43ecf0787d52462872a3526650656daebb161aca
Anders Andersson <trezker@gmail.com>
2016-11-12 18:14:58 +0100

    Bound function to sign up

--- public/source/js/app/loginViewModel.js
+++ public/source/js/app/loginViewModel.js
@@ -1,10 +1,18 @@
 var loginViewModel = {
 	username: '',
 	password: '',
-	log_in : function() {
+	sign_in : function() {
 		var data = ko.toJS(this);
 		data.model = "user";
 		data.method = "login_password";
+		ajax_post(data, function(returnedData) {
+		    console.log(returnedData);
+		});
+	},
+	sign_up : function() {
+		var data = ko.toJS(this);
+		data.model = "user";
+		data.method = "create_user";
 		ajax_post(data, function(returnedData) {
 		    console.log(returnedData);
 		});
